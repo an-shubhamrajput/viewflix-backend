39385b897cf7ef5cfad5883a3bf0f2011f4d0b29
an-shubhamrajput <shubham.r@applicationnexus.com>
2025-11-07 11:18:41 +0530

    top & popular movie api

--- route/movie.py
+++ route/movie.py
@@ -14,9 +14,50 @@ def movieInfo():
     except ValueError as e:
         return jsonify({"error": str(e)}), 400
     cursor = conn.cursor(dictionary=True)
-    cursor.execute('SELECT * FROM top_rated_en')
+    cursor.execute('SELECT * FROM popularmovies limit 60')
     movies = cursor.fetchall()
     cursor.close()
     session['username'] = 'shubham rajput'
     session['11'] = 123
     return {"movies": movies}
+
+
+@movie_bp.route('/recent-movies',methods=['GET'])
+def top_movies():
+    print("Inside recent movies")
+    print("Request args:", request.args)
+    genre_id = request.args.get('genre_id')
+    if not genre_id:
+        return jsonify({"error": "genre_id query param is required"}), 400
+    print("genre_id:", genre_id)
+    try:
+        conn = get_db_for_genre(genre_id)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM recent_added_movies limit 10')
+    movies = cursor.fetchall()
+    cursor.close()
+    return {"movies": movies}
+
+
+@movie_bp.route('/popular',methods=['GET'])
+def popular_movies():
+    print("Inside recent movies")
+    print("Request args:", request.args)
+    genre_id = request.args.get('genre_id')
+    if not genre_id:
+        return jsonify({"error": "genre_id query param is required"}), 400
+    print("genre_id:", genre_id)
+    try:
+        conn = get_db_for_genre(genre_id)
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute('SELECT * FROM popularmovies limit 10')
+    movies = cursor.fetchall()
+    cursor.close()
+    return {"movies": movies}
+
+
+
